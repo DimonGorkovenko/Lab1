94985d491a61a3385420302f348ee03a971bc393
DimonGorkovenko <114645644+DimonGorkovenko@users.noreply.github.com>
2022-10-02 10:18:02 +0300

    Add files via upload

---
+++ Lab1_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    float a, b, c, S, V;
+    printf("Hello, World!\nEnter 3 parallelepiped edges:\n");
+    scanf_s("%f", &a);
+    scanf_s("%f", &b);
+    scanf_s("%f", &c);
+    if (a <= 0 || b <= 0 || c <= 0) {
+        printf("Incorrect numbers");
+        return 0;
+    }
+    else {
+        V = a * b * c;
+        printf("The space is equal %.3f\n", V);
+        S = 2 * a * b + 2 * a * c + 2 * b * c;
+        printf("Full area is equal %.3f", S);
+        return 0;
+    }
+}
+
